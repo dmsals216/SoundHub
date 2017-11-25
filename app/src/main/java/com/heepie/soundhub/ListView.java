6c5039b2f13e6b1511b1630412401fb7d49d34a6
heepie <gheepie@gmail.com>
2017-11-25 16:19:38 +0900

    First Commit

---
+++ app/src/main/java/com/heepie/soundhub/ListView.java
@@ -0,0 +1,13 @@
+package com.heepie.soundhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_list);
+    }
+}
