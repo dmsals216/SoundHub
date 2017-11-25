ad02b542a21d1bf4fcfb496c5bf9a0e3ca1fcb9a
heepie <gheepie@gmail.com>
2017-11-25 16:54:12 +0900

    MVVM 설정 사항 추가

--- app/src/main/java/com/heepie/soundhub/view/ListView.java
+++ app/src/main/java/com/heepie/soundhub/view/ListView.java
@@ -1,7 +1,9 @@
-package com.heepie.soundhub;
+package com.heepie.soundhub.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.heepie.soundhub.R;
 
 public class ListView extends AppCompatActivity {
 
