7f48f10a89d46461a23bb3669a773338311105a9
DESKTOP-S6F7VQ4\eunmin <dmsals2160@gmail.com>
2017-11-27 19:04:31 +0900

    google, naver setting

--- app/src/main/java/com/heepie/soundhub/viewmodel/InputViewModel.java
+++ app/src/main/java/com/heepie/soundhub/viewmodel/InputViewModel.java
@@ -10,37 +10,37 @@ import android.view.View;
 
 public class InputViewModel {
     public final String TAG = getClass().getSimpleName();
-    public final ObservableField<Boolean> showSignUp = new ObservableField<>(false);
-    public final ObservableField<Boolean> showSignUpOption = new ObservableField<>(false);
-    public final ObservableField<Boolean> showSignIn = new ObservableField<>(false);
+    public boolean showSignUp = false;
+    public boolean showSignUpOption = false;
+    public boolean showSignIn = false;
 
     public void onClickedSignUp(View v) {
-        if (showSignUpOption.get())
-            showSignUpOption.set(false);
+        if (showSignUpOption)
+            showSignUpOption = false;
         else
-            showSignUpOption.set(true);
-        Log.d(TAG, "showSignUp: " + showSignUp.get());
+            showSignUpOption = true;
+        Log.d(TAG, "showSignUp: " + showSignUp);
     }
 
     public void onClickedSignIn(View v) {
-        if (showSignIn.get())
-            showSignIn.set(false);
+        if (showSignIn)
+            showSignIn = false;
         else
-            showSignIn.set(true);
-        Log.d(TAG, "showSignUp: " + showSignIn.get());
+            showSignIn = true;
+        Log.d(TAG, "showSignUp: " + showSignIn);
     }
 
     public void onClickedSignUpOption(View v) {
-        if (showSignUp.get())
-            showSignUp.set(false);
+        if (showSignUp)
+            showSignUp = false;
         else
-            showSignUp.set(true);
-        Log.d(TAG, "showSignUp: " + showSignUp.get());
+            showSignUp = true;
+        Log.d(TAG, "showSignUp: " + showSignUp);
     }
 
     public void onClickedFrame(View v) {
-        showSignUpOption.set(false);
-        showSignIn.set(false);
-        showSignUp.set(false);
+        showSignUpOption = false;
+        showSignIn = false;
+        showSignUp = false;
     }
 }
