2b0658659c0ca6918053d90acba869136e81c355
heepie <gheepie@gmail.com>
2017-11-28 21:42:08 +0900

    네이게이션 뷰 추가 완료(추후 데이터 바인딩으로 변경 필요)

--- app/src/main/java/com/heepie/soundhub/view/ListView.java
+++ app/src/main/java/com/heepie/soundhub/view/ListView.java
@@ -1,12 +1,16 @@
 package com.heepie.soundhub.view;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
 
 import com.heepie.soundhub.R;
 import com.heepie.soundhub.databinding.ListViewBinding;
+import com.heepie.soundhub.databinding.NavigationHeaderBinding;
 import com.heepie.soundhub.model.User;
 import com.heepie.soundhub.viewmodel.ListViewModel;
 import com.heepie.soundhub.viewmodel.PostsViewModel;
@@ -19,6 +23,17 @@ public class ListView extends AppCompatActivity {
 
         ListViewBinding listBinding = DataBindingUtil.setContentView(this, R.layout.list_view);
         ListViewModel listViewModel = new ListViewModel();
+
+        // 데이터 바인딩으로 변경해야 함
+        // 네비게이션 뷰(액션바 햄버거) 설정
+        NavigationHeaderBinding navigationViewHeaderBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.navigation_header,listBinding.navigation,false);
+        listBinding.navigation.addHeaderView(navigationViewHeaderBinding.getRoot());
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, listBinding.drawerLayout, listBinding.toolbar, 0, 0);
+//            ((DrawerLayout)view).addDrawerListener(toggle);
+        toggle.syncState();
+
 
         // 더미 데이터 생성
         for (int i=0; i<20; i=i+1) {
