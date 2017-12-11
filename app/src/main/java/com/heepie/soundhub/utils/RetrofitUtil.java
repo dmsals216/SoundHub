93a33e3870f4e9d5aae57b117997a4cda2740836
DESKTOP-S6F7VQ4\eunmin <dmsals2160@gmail.com>
2017-12-11 10:17:49 +0900

    레트로핏 로그 추가

--- app/src/main/java/com/heepie/soundhub/utils/RetrofitUtil.java
+++ app/src/main/java/com/heepie/soundhub/utils/RetrofitUtil.java
@@ -1,23 +1,34 @@
 package com.heepie.soundhub.utils;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by eunmin on 2017-12-04.
  */
-
 public class RetrofitUtil {
     private static String SERVER_URL = "https://soundhub.che1.co.kr";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
         if(null == retrofit) {
+            // OkHttp Client 생성
+            HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+            logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient okClient = new OkHttpClient
+                    .Builder()
+                    .addInterceptor(logInterceptor)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(SERVER_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okClient)
                     .build();
         }
+
         return retrofit;
     }
 }
